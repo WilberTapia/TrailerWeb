e64a208690f717e24881dabb7ff943d71d0ce506
WTapia <wtapia425@gmail.com>
2019-08-03 08:30:25 +0400

    FInal
    FInal

--- Trailer/application/views/formulario.php
+++ Trailer/application/views/formulario.php
@@ -11,12 +11,10 @@ $peliculas->fechala ='';
 $peliculas->imagen ='';
 $peliculas->trailerid ='';
 
-  //var_dump($datos);
 if (isset($datos)) {
   if (count($datos) > 0) {
     $peliculas = $datos[0];
   }
-  // code...
 }
 ?>
 <!DOCTYPE html>
@@ -32,7 +30,7 @@ if (isset($datos)) {
     <div class="col-sm-6">
         <div class="form-row mb-4">
             <div class="col-md-12">
-              <input type="hidden" name="id" class="form-control" value="<?php echo $peliculas->id; ?>" required>
+              <input type="hidden" name="id" class="form-control" value="<?= $peliculas->id; ?>" required>
                 <label>Titulo:</label>
                 <input type="text" name="titulo" class="form-control" placeholder="Titulo..." value="<?= $peliculas->titulo; ?>" required>
             </div>
@@ -41,35 +39,35 @@ if (isset($datos)) {
         <div class="form-row mb-4">
             <div class="col-md-4">
                 <label>Duracion:</label>
-                <input type="text" name="duracion" class="form-control" placeholder="Duracion..." value="<?php echo $peliculas->duracion; ?>" required>
+                <input type="text" name="duracion" class="form-control" placeholder="Duracion..." value="<?= $peliculas->duracion; ?>" required>
             </div>
 
             <div class="col-md-4">
                 <label>Genero:</label>
-                <input type="text" name="genero" class="form-control" placeholder="Genero..." value="<?php echo $peliculas->genero; ?>" required>
+                <input type="text" name="genero" class="form-control" placeholder="Genero..." value="<?= $peliculas->genero; ?>" required>
             </div>
 
             <div class="col-md-4">
                 <label>Fecha de lanzamiento:</label>
-                <input type="date" name="fechala" class="form-control" placeholder="Fecha de Lanzamiento" value="<?php echo $peliculas->fechala; ?>" required>
+                <input type="date" name="fechala" class="form-control" placeholder="Fecha de Lanzamiento" value="<?= $peliculas->fechala; ?>" required>
             </div>
         </div>
 
         <div class="form-row mb-4">
               <div class="col-md-12">
                   <label>Actores:</label>
-                  <input type="text" name="actores" class="form-control" placeholder="Actores..." value="<?php echo $peliculas->actores; ?>" required>
+                  <input type="text" name="actores" class="form-control" placeholder="Actores..." value="<?= $peliculas->actores; ?>" required>
               </div>
           </div>
         <div class="form-row mb-4">
             <div class="col-md-6">
                 <label>Link de la Imagen:</label>
-                <input type="text" id="imagen" onblur="cargarImagen();" onload="cargarImagen();" name="imagen" class="form-control"  value="<?php echo $peliculas->imagen; ?>" required placeholder="Link de la Imagen...">
+                <input type="text" id="imagen" onblur="cargarImagen();" onload="cargarImagen();" name="imagen" class="form-control"  value="<?= $peliculas->imagen; ?>" required placeholder="Link de la Imagen...">
             </div>
 
             <div class="col-md-6">
                 <label>Link del Trailer:</label>
-                <input type="text" name="trailerid" class="form-control" placeholder="Link de Trailer..." value="<?php echo $peliculas->trailerid; ?>" required>
+                <input type="text" name="trailerid" class="form-control" placeholder="Link de Trailer..." value="<?= $peliculas->trailerid; ?>" required>
             </div>
         </div>
 
@@ -82,7 +80,7 @@ if (isset($datos)) {
     <div class="col-sm-4 ml-auto">
             <div class="card">
                 <div class="card-body">
-                    <img id="img" src="" class="img-fluid"/>
+                    <img id="img" src=""  alt="Aparecera la imagen Luego de que coloque el link de la Imagen." class="img-fluid"/>
                 </div>
             </div>
     </div>
